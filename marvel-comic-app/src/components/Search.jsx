32a418ef773050e9d66af2dc0dac960826fa8d38
Mohammad Haider <arzhdr@gmail.com>
2024-06-03 23:40:35 -0400

    implemented submit and made functions to get data

--- marvel-comic-app/src/components/Search.jsx
+++ marvel-comic-app/src/components/Search.jsx
@@ -3,13 +3,32 @@ import { useState } from "react";
 
 const Search = () => {
   const [characterName, setCharacterName] = useState("");
+  const [characterData, setCharacterData] = useState(null);
+  const [comicData, setComicData] = useState(null);
+
+  const publicKey = import.meta.env.VITE_PUBLIC_KEY;
+  const privateKey = import.meta.env.VITE_PRIVATE_KEY;
 
   const handleSubmit = () => {
-    //TODO implement later
+    event.preventDefault();
+
+    getCharacterData();
+  };
+
+  const getCharacterData = () => {
+    //resets data if an API call doesnt go through
+    setCharacterData(null);
+    setComicData(null);
+
+    const timeStamp = new Date().getTime();
+    const hash = generateHash(timeStamp);
+  };
+
+  const generateHash = (timeStamp) => {
+    return md5(timeStamp + publicKey + privateKey);
   };
 
   const handleChange = (event) => {
-    //TODO implement later
     setCharacterName(event.target.value);
   };
 
